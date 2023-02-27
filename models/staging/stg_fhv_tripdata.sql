77d00e9c58f432653790af2412a7b783f2665785
ing.juan.fco.hdz@gmail.com <ing.juan.fco.hdz@gmail.com>
2023-02-27 05:09:26 +0000

    Updating fact fhv table

---
+++ models/staging/stg_fhv_tripdata.sql
@@ -0,0 +1,16 @@
+{{ config(materialized='view') }}
+
+select 
+    {{ dbt_utils.surrogate_key(['dispatching_base_num', 'pickup_datetime']) }} as tripid,
+    -- trip info
+    dispatching_base_num,
+    -- timestamps
+    cast(pickup_datetime as timestamp) as pickup_datetime,
+    cast(dropOff_datetime as timestamp) as dropoff_datetime,
+    -- trip info
+    cast(PUlocationID as integer) as  pickup_locationID,
+    cast(DOlocationID as integer) as dropoff_locationID,
+    cast(SR_Flag as numeric) as SR_Flag,
+    Affiliated_base_number as affiliated_base_number
+
+from {{ source('staging', 'fhvTaxiParquet') }}
--- models/staging/stg_yellow_tripdata.sql
+++ models/staging/stg_yellow_tripdata.sql
@@ -4,7 +4,7 @@ with tripdata as
 (
   select *,
     row_number() over(partition by vendorid, tpep_pickup_datetime) as rn
-  from {{ source('staging','yellow_tripdata') }}
+  from {{ source('staging','yellowTaxiParquet') }}
   where vendorid is not null 
 )
 select
@@ -42,8 +42,8 @@ from tripdata
 where rn = 1
 
 -- dbt build --m <model.sql> --var 'is_test_run: false'
-{% if var('is_test_run', default=true) %}
+--{% if var('is_test_run', default=true) %}
 
-  limit 100
+--  limit 100
 
-{% endif %}
+--{% endif %}
